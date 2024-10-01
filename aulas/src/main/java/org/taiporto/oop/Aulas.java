2f666df5134597463e124d6a6bdb86cc449c21e6
Tai <tai.dporto@gmail.com>
2024-09-30 22:47:54 -0300

    create java project and start exercise 2

---
+++ aulas/src/main/java/org/taiporto/oop/Aulas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.taiporto.oop;
+
+/**
+ *
+ * @author taiporto
+ */
+public class Aulas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
